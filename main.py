50b7611ed97d21413da7df820630cd29fb63d071
Kyle Ricks <ricksk3@wwu.edu>
2015-11-24 15:02:44 -0800

    added main.py; beginning classes

---
+++ main.py
@@ -0,0 +1,45 @@
+# Powers - Original Beginning Scope is to be able to query Powers from a SQLite DB
+# Potential to be used as a spring board for other offerings
+#
+# Original Author: Kyle Ricks
+# Organization: Shadow Accord - NW LARPers
+#
+# Imports
+#
+import sys
+import sqlite3
+#
+#
+#q_type = input("Please select from the below menu something you would like to query:\\r
+#    [1] Power Name \\r
+#    [2] Tree Name \r
+#    [3] Type (Touch, Damage, Self, Mental, Other, Status, Mask, Passive, Sensory) \r
+#    [4] Meta Type \r
+#    [5] ST Only \r
+#    [6] Breachable Powers")
+#
+# class Menu: will also go here in time
+#
+class DB:
+    def __init__(self):
+        pass
+    def power_name(self):
+        pass
+    def tree_name(self):
+        pass
+    def power_type(self):
+        pass
+    def meta_type(self):
+        pass
+    def st_only(self):
+        pass
+    def breach(self):
+        pass
+#
+#
+if __init__ = "__main__":
+# Database Connection
+# https://docs.python.org/3.5/library/sqlite3.html
+#
+    conn = sqlite3.connect('Powers_20151116.sqlite')
+
