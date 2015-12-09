6eca2924daabdf8d70a9af9352a59ebd9938ab46
Kyle Ricks <ricksk3@wwu.edu>
2015-12-09 14:43:40 -0800

    All working!

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class DB_Functions:
             print(row)
     def power_type(self):
         pow_type = input("Please type in a comma separated list of each power type you would like to see.  Touch, Damage, Self, Mental, Other, Status, Mask, Passive, Sensory.:  ")
-        type_list = (("".join(meta.split())).split(','))
+        type_list = (("".join(pow_type.split())).split(','))
         for selection in type_list:
             sel = (selection.upper(),)
             for row in self.c.execute('''SELECT DISTINCT power_name,type,meta,cost,call,text,st_only,breachable FROM powers WHERE upper(type)=?''', sel):
@@ -68,6 +68,7 @@ class Menu:
             self.db.breach()
         else:
             print("You have not selected a valid option.  Please select again.")
+            selection = input("Please select what function you would like.\n\t[1] Search by name(s)\n\t[2] Search by Tree\n\t[3] Search by Power Type\n\t[4] Search by Power Meta\n\t[5] View all ST_Only power\n\t[6] View all powers that are Breachable\n\n")
             self.selector(selection)
 #
 #
