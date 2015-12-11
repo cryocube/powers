22c66a895ab76bf699372e85c2da2441212e5867
Kyle Ricks <kyle.ricks@wwu.edu>
2015-12-10 16:17:44 -0800

    Sanitization work.  Working on setting up name search to handle multiple requests and to handle using _ to replace spaces in input to help make input more uniform.

--- main.py
+++ main.py
@@ -21,28 +21,47 @@ class DB_Functions:
         self.c = self.conn.cursor()
     def power_name(self):
         query = '''SELECT DISTINCT powers.power_name as Power, trees.tree_name as Tree, power_trees.tier as Tree_Tier, factions.subfaction as Splat from innate_tree join factions on innate_tree.subfaction_id=factions.subfaction_id join trees on trees.tree_id=innate_tree.tree_id join power_trees on power_trees.tree_id=trees.tree_id join powers on powers.power_id=power_trees.power_id WHERE upper(powers.power_name) =?'''
-        pname = input("Please type in the name of the Power you would like to see the information of:  ")
-        power = (pname.upper(),)
-        for row in self.c.execute(query, power):
-            print(row)
+        names=("absolution","aggravated_1","aggravated_claws","appear","avert","avoidance","balefire","beast_mind","black_ichor","body_wrack","break_attunement","brittle_bones","brutal_strike","clawed_form","cleanse","cloak","cloak_gathering","cloak_sight","cognizance","conditioning","confusion","control_body","control_voice","corrupted_powers","craving","dark_sword","daze","decay","derange","despair","detect_fetter","detect_taint","disable","disarm","disembodied","disquiet","dreamshape","endure","entrancement","exorcism","expel_corpus","fabricate_armor","fast_healing","fetter_consumption","fetter_creation","fire_2","fire_4","fire_sword","forgetful_mind","form_of_vapor","frenzy","frenzy_control","gauntlet_walk","give_energy","god's_grace","grant_power","hallucination","hasty_escape","healing_touch","health_exchange","hellborn_investiture","hero's_stand","hidden_taint","hide_of_the_wyrm","horrid_reality","hypnotism","imitate","induce_catharsis","induce_sin","insight","leech_of_fear","light_sword","majesty","mask_of_a_thousand_faces","materialize","meditate","meld","might","mimic","monsters","move_object","obedience","paralyze","passion","pathos_exchange","pathos_investment","pence_from_heaven","poison_immunity","possession","powerful_form","purify","ranged_2","ranged_4","razor_claws","read_magic","realm_grasp","release_spirit","rend_the_lifeweb","resilience","resist_gauntlet","resist_taint","restore_essence","revive","root","sanctuary","scion_of_evil","secret_angst","sense_amaranth","sense_confidence","sense_demon","sense_desire","sense_emotion","sense_essence","sense_faction","sense_fetter","sense_gnosis","sense_health","sense_item","sense_maximum_health","sense_mental","sense_patron","sense_power","sense_shadow","sense_spirit","sense_subfaction","sense_vitae","serenity","shadow_coax","shatter","shunt","silence","silver_armor","silver_claws","silver_tongue","smell_fear","snarl","song_of_rage","spirit_drain","stonehand_punch","strength","subjugate","taint","taunt","telepathy","tentacles","terror","test_generation","test_oath","totemic_form","tough_form","toughness","true_form","umbra_drain","umbra_sight","umbra_strike","vengeance_of_samiel","venom","visions","weaponry","wither","withstand","woadling","wounding_lies")
+        pname = input("Please type in the names of the Powers (if it is two words, replace the space with a _) you would like to see the information of, in a comma separated list:  ")
+        proc_pname = (Sanitizer().sanitize_pname(pname)).lower()
+        pow_list = ("".join(proc_pname.split())).split(',')
+        print("\n\nINTERMEDIATE pow_list\n\n" + str(pow_list))
+        pow_list = [v for v in pow_list if v in names]
+        print("Displaying all text for: " + str(pow_list))
+        for v in pow_list:
+            power = ((str(v).upper()),)
+            print(str(power))
+            for row in self.c.execute(query, power):
+                print(row)
     def tree_name(self):
         query = '''SELECT DISTINCT powers.power_name as Power, trees.tree_name as Tree, power_trees.tier as Tree_Tier, factions.subfaction as Splat from innate_tree join factions on innate_tree.subfaction_id=factions.subfaction_id join trees on trees.tree_id=innate_tree.tree_id join power_trees on power_trees.tree_id=trees.tree_id join powers on powers.power_id=power_trees.power_id WHERE upper(trees.tree_name) =?'''
         tname = input("Please type in the name of the Tree you would like to see the powers of:  ")
-        tree = (tname.upper(),)
+        proc_tname = Sanitizer().sanitize_input(tname)
+        tree = (proc_tname.upper(),)
         for row in self.c.execute(query, tree):
             print(row)
     def power_type(self):
         query = '''SELECT DISTINCT power_name,type,meta,cost,call,text,st_only,breachable FROM powers WHERE upper(type)=?'''
-        pow_type = input("Please type in a comma separated list of each power type you would like to see.  Touch, Damage, Self, Mental, Other, Status, Mask, Passive, Sensory.:  ")
+        types = ('touch', 'damage', 'self', 'mental', 'other', 'status', 'mask', 'passive', 'sensory')
+        pow_type = input("Please type in a comma separated list of each power type you would like to see.\nOptions are:\n " + str(types) + "  :  ")
+        proc_type = Sanitizer().sanitize_input(pow_type)
         type_list = (("".join(pow_type.split())).split(','))
+        # Note: Using list comprehension.  Note for self.        
+        type_list = [v for v in type_list if v in types]
+        print("Displaying all options for: " + str(type_list))
         for selection in type_list:
             sel = (selection.upper(),)
             for row in self.c.execute(query, sel):
                 print(row)
     def meta_type(self):
         query = '''SELECT DISTINCT power_name,type,meta,cost,call,text,st_only,breachable FROM powers WHERE upper(meta)=?'''
-        meta = input("Please type in a comma separated list of each meta type you would like to see. None, Meta, Condition, Type, Counted.:  ")
-        meta_list = (("".join(meta.split())).split(','))
+        metas = ('none', 'meta', 'condition', 'type', 'counted')
+        meta = input("Please type in a comma separated list of each meta type you would like to see.\nOptions are:\n " + str(metas) + "  :  ")
+        proc_meta = Sanitizer().sanitize_input(meta)
+        meta_list = (("".join(proc_meta.split())).split(','))
+        # Note: Using list comprehension.  Note for self.
+        meta_list = [v for v in meta_list if v in metas]
+        print("Displaying all options for: " + str(meta_list))
         for selection in meta_list:
             sel = (selection.upper(),)
             for row in self.c.execute(query, sel):
@@ -80,6 +99,29 @@ class Menu:
             print("You have not selected a valid option.  Please select again.")
             sel = Menu(db).prompt()
             self.selector(sel)
+#
+#
+class Sanitizer:
+    def __init__(self):
+        pass
+    def sanitize_input(self, raw_string):
+        string = str()
+        slist = list()
+        for c in raw_string:
+            if ord(c) == 32 or ord(c) == 44 or 65<=ord(c)<=90 or 97<=ord(c)<=122:
+                string += c
+            else:
+                string += ''
+        return string
+    def sanitize_pname(self, raw_string):
+        string = str()
+        slist = list()
+        for c in raw_string:
+            if ord(c) == 39 or ord(c) == 95 or ord(c) == 32 or ord(c) == 44 or 48<=ord(c)<=57 or 65<=ord(c)<=90 or 97<=ord(c)<=122:
+                string += c
+            else:
+                string += ''
+        return string
 #
 #
 if __name__ == "__main__":
